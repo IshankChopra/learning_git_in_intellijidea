28d9faebc31312924d7ce6009b5850e3064cfddf
Ishank Chopra <ishankchopranbd@gmail.com>
2024-06-13 19:45:08 +0530

    initial git project

---
+++ src/main/java/dev/spring/spring_project/SpringProjectLearningApplication.java
@@ -0,0 +1,15 @@
+package dev.spring.spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectLearningApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/dev/spring/spring_project/SpringProjectLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
